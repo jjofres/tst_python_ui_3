6b6e46bf62ce2d6f54eafe0073e24302ed5a40e4
Javier <jovier@gmail.com>
2023-03-08 09:34:42 -0500

    some commit

--- tst_python_ui_3/widget.py
+++ tst_python_ui_3/widget.py
@@ -8,14 +8,14 @@ from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
 
 
-class Widget(QWidget):
+class First(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.load_ui()
 
     def load_ui(self):
         loader = QUiLoader()
-        path = Path(__file__).resolve().parent / "form.ui"
+        path = Path(__file__).resolve().parent / "formFirst.ui"
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
         loader.load(ui_file, self)
@@ -24,6 +24,6 @@ class Widget(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    widget = Widget()
+    widget = First()
     widget.show()
     sys.exit(app.exec())
